98e5bc8c132d283356c024b31903f665e3ebd39b
Gabriela Torres <gabrielatorres@Interapts-MacBook-Pro-6.local>
2018-09-10 12:45:12 -0500

    Committing intial files

---
+++ Video Game Library/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Video Game Library
+//
+//  Created by Gabriela Torres on 9/10/18.
+//  Copyright © 2018 Gabriela Torres. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
